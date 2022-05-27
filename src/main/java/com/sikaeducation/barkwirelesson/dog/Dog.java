3d251c57d4ae5108cfa5e06fee7b79ac594c712f
Kyle Coberly <kyle.coberly@gmail.com>
2022-05-27 11:31:29 -0600

    Initial commit

---
+++ src/main/java/com/sikaeducation/barkwirelesson/dog/Dog.java
@@ -0,0 +1,25 @@
+package com.sikaeducation.barkwirelesson.dog;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "dog")
+public class Dog {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "breed")
+  private String breed;
+}
---
+++ src/main/java/com/sikaeducation/barkwirelesson/dog/DogRepository.java
@@ -0,0 +1,7 @@
+package com.sikaeducation.barkwirelesson.dog;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DogRepository extends CrudRepository<Dog, Long> {}
